197b3f607788ea88f5c75f401440581aac4a6d20
Nasiha-03 <nasihavlr2003@gmail.com>
2025-08-06 23:27:51 +0530

    Fix: Corrected download report API route

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,20 +1,13 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_API_URL,
-  withCredentials: true,
-});
-
 export const downloadReport = async (reportData: any) => {
   const token = localStorage.getItem('token');
-  return api.post('/download-report', reportData, {
+  return api.post('/api/download-report', reportData, {
     headers: { Authorization: `Bearer ${token}` },
     responseType: 'blob',
   });
 };
 
-export const fetchAssessments = async (token: string): Promise<any> => {
-  return api.get('/assessments', {
+export const fetchAssessments = async (token: string) => {
+  return api.get('/api/assessments', {
     headers: { Authorization: `Bearer ${token}` },
   });
 };
